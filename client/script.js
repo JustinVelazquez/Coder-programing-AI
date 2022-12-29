e969bbfd56bba70bbf8b4b4038d9337cd9afc293
Justin V <justinv4994@gmail.com>
2022-12-29 10:45:11 -0500

    updated client-side to distingush between user/ai chat prompts

--- client/script.js
+++ client/script.js
@@ -2,7 +2,7 @@ import bot from './assets/bot.svg';
 import user from './assets/user.svg';
 
 const form = document.querySelector('form');
-const container = document.querySelector('#chat_container');
+const chatContainer = document.querySelector('#chat_container');
 
 let loadInterval;
 
@@ -42,8 +42,59 @@ function generateUniqueId() {
 //   return (
 //     `
 //     <div class='wrapper ${isAi && 'ai'}>
+//     <div class='chat'>
+//     <div class='profile'>
+//     <img src='${isAi ? bot : user}' alt='${isAi ? 'bot' : 'user'}'/>
+//     </div>
+//     <div class='message' id=${uniqueId}>${value} </div>
+//     </div>
 
 //     </div>
 //     `
 //   )
 // }
+function chatStripe(isAi, value, uniqueId) {
+  return (
+  `
+      <div class="wrapper ${isAi && 'ai'}">
+          <div class="chat">
+              <div class="profile">
+                  <img 
+                    src=${isAi ? bot : user} 
+                    alt="${isAi ? 'bot' : 'user'}" 
+                  />
+              </div>
+              <div class="message" id=${uniqueId}>${value}</div>
+          </div>
+      </div>
+  `
+  )
+}
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+
+  const data = new FormData(form);
+
+  // user chatStripe
+  chatContainer.innerHTML += chatStripe(false, data.get('prompt'));
+
+  form.reset();
+
+  //bot chatStripe
+  const uniqueId = generateUniqueId();
+  chatContainer.innerHTML += chatStripe(true, ' ', uniqueId);
+
+  chatContainer.scrollTop = chatContainer.scrollHeight;
+
+  const messageDiv = document.getElementById(uniqueId);
+
+  loader(messageDiv);
+};
+
+form.addEventListener('submit', handleSubmit);
+form.addEventListener('keyup', (e) => {
+  if (e.keycode === 13) {
+    handleSubmit(e);
+  }
+});
