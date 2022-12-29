92dafb73f764663706b2874b865b77a40b8a9375
Justin V <justinv4994@gmail.com>
2022-12-29 13:41:35 -0500

    updated server call

--- client/script.js
+++ client/script.js
@@ -38,8 +38,8 @@ function generateUniqueId() {
   return `id-${timeStamp}-${hexString}`;
 }
 function chatStripe(isAi, value, uniqueId) {
-  return (
-    `
+  return ( 
+  `
       <div class="wrapper ${isAi && 'ai'}">
           <div class="chat">
               <div class="profile">
@@ -76,7 +76,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   //fetch data from server
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://codex-dihb.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
